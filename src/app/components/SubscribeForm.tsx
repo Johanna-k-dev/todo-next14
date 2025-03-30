dd2445d489bd331764ceb35e11f82c60e569bc21
Johanna Keïta <veille.technologique.jk@gmail.com>
2025-03-30 20:10:10 +0200

    Addes Subscribe/Login form and prisma import in page.tsx

---
+++ src/app/components/SubscribeForm.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+export default function SubscribeForm() {
+  return (
+    <div className='flex flex-row items-center justify-center h-screen'>
+      <form className="flex flex-col items-center border border-blue-300 rounded p-2 mb-4">
+        <h2>Inscription</h2>
+            <input 
+            type="text"
+            placeholder="Nom"
+            className="border border-gray-300 rounded p-2 mb-4"
+            />
+            <input
+            type="email"
+            placeholder="Email"
+            className="border border-gray-300 rounded p-2 mb-4"
+            />  
+            <input
+            type="password"
+            placeholder="Mot de passe"
+            className="border border-gray-300 rounded p-2 mb-4"
+            />
+            <button type='submit' className="bg-blue-500 text-white rounded p-2 hover:bg-blue-300">S'inscrire</button>
+      </form>
+      <form  className="flex flex-col items-center border border-blue-300 rounded p-2 mb-4">
+            <h2>Connection</h2>
+            <input
+            type="email"
+            placeholder="Email"
+            className="border border-gray-300 rounded p-2 mb-4"
+            />
+            <input
+            type="password"
+            placeholder="Mot de passe"
+            className="border border-gray-300 rounded p-2 mb-4"
+            />
+            <button type='submit' className="bg-blue-500 text-white rounded p-2 hover:bg-blue-300">Se connecter</button>
+        </form>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 'use client';
-
+import { prisma } from '@lib/prisma';
 import CreateToDoList from "./components/CreateToDoList";
 import TaskForm from "./components/TaskForm";
+import SubscribeForm from './components/SubscribeForm';
 
 export default function Home() {
 
   return (
     <main>
+      <SubscribeForm/>
       <h1 className="p-4 max-w-xl mx-auto text-3xl font-bold mb-4">📝 Ma Super Todo List</h1>
       <section>
         <TaskForm/>
